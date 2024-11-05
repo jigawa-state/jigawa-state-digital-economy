ee0a379b6d93f1b52a883c20999298e3ac2a5fc8
doudgaya <arbdoule@yahoo.com>
2024-11-05 21:55:35 +0100

    es-lint err

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,16 +18,13 @@ export default function AboutUs() {
       {/* Hero Section */}
       <section className="relative bg-green-500 py-20 h-[70vh] pt-20 px-4 sm:px-6 lg:px-8">
         <div className="relative max-w-7xl space-y-4 items-center pt-20 mx-auto">
-            <Image src={jigawaLogo.src} alt='' className=' h-32 object-contain max-w-max object-left' width={700} height={700} />
+            <Image src={jigawaLogo} alt='Jigawa State Logo' className=' h-32 object-contain max-w-max object-left' width={700} height={700} />
            <h1 className="text-2xl font-bold font-poppins tracking-tight text-gray-900 sm:text-2xl md:text-4xl">
             Welcome to <span className="text-white">Jigawa State</span> ICT and Digital Economy
           </h1>
           <p className="mt-6 max-w-3xl text-xl text-gray-800">
             We are committed to providing digital services and infrastructure to the people of Jigawa State.
           </p>
-          {/* <div className="mt-10">
-            <Button className="bg-gray-900 text-white hover:bg-gray-800">Explore Our Listings</Button>
-          </div> */}
         </div>
       </section>
 
