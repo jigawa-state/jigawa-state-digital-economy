dc71d40d6b74ad92f88cb8f881640dfb4451169a
DoudGaya <arboule@yahoo.com>
2024-12-12 22:25:27 +0100

    nep

--- app/(auth)/new-password/page.tsx
+++ app/(auth)/new-password/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { NewPasswordForm } from '@/components/auth/NewPasswordForm'
 
 const page = () => {
   return (
     <div>
-        <NewPasswordForm />
+        <Suspense fallback={<div>Loading...</div>}>
+            <NewPasswordForm />
+        </Suspense>
+        {/* <NewPasswordForm /> */}
     </div>
   )
 }
