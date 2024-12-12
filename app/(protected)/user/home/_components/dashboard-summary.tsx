e2943e6df7915770a968e2dffc6f21bfb7e5edae
DoudGaya <arboule@yahoo.com>
2024-12-13 00:33:53 +0100

    drib

--- app/(protected)/user/home/_components/dashboard-summary.tsx
+++ app/(protected)/user/home/_components/dashboard-summary.tsx
@@ -1,22 +1,33 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { ActivitiesType, GalleryType, NewsType, PoliciesType } from '@/typings'
+import Link from 'next/link'
 // import { fetchSummary } from '@/app/actions'
 
-interface DashboardSummaryProps {
-  type: 'policies' | 'galleries' | 'news' | 'activities'
-}
+// interface DashboardSummaryProps {
+//   type: PoliciesType[] | GalleryType[] | NewsType[] | ActivitiesType[]
+// }
 
-export async function DashboardSummary({ type }: DashboardSummaryProps) {
+export async function DashboardSummary({ 
+    count,
+    title,
+ }: {
+    count: number
+    title: string
+}) {
 //   const summary = await fetchSummary(type)
   return (
-    <Card className=' bg-white dark:bg-black/30'>
+    <Card className=' bg-white space-y-2  dark:bg-black/30'>
       <CardHeader>
-        <CardTitle className="capitalize">
-            {/* {type} */}
+        <CardTitle className="capitalize flex items-center justify-between">
+            <p className=''>{title}</p>
+            <p className='text-3xl font-bold'>{count}</p>
         </CardTitle>
       </CardHeader>
-      <CardContent>
-       
-      </CardContent>
+            <Link className='' href={`/user/${title.toLocaleLowerCase()}`}> 
+                <CardFooter className=' w-full border-t py-1' >
+                    View All
+                </CardFooter>
+            </Link>
     </Card>
   )
 }
--- app/(protected)/user/home/page.tsx
+++ app/(protected)/user/home/page.tsx
@@ -2,27 +2,72 @@ import { getAllAuthors } from '@/actions/author'
 import { getAllNews } from '@/actions/news'
 import React from 'react'
 import { Suspense } from 'react'
-// import { DashboardSummary } from '@/'
+
 import { DashboardSummary } from './_components/dashboard-summary'
 import { CardSkeleton } from './_components/card-skeleton'
-// import { CardSkeleton } from '@/components/card-skeleton'
+import { getAllRecords } from '@/actions/dashboard'
 
 const HomeDashboard = async () => {
+
+
+
+  const {
+    policies,
+    galleries,
+    news,
+    activities
+  } = await getAllRecords()
+
+
+
+  type CardType = {
+    id: number;
+    title: string;
+    count: number;
+  }
+
+
+  const cards: CardType[] = [
+    {
+      id: 1,
+      title: "Policies",
+      count: policies.length
+    },
+    {
+      id: 2,
+      title: "Galleries",
+      count: galleries.length
+    },
+    {
+      id: 3,
+      title: "News",
+      count: news.length
+    },
+    {
+      id: 4,
+      title: "Activities",
+      count: activities.length
+    }
+  ]
+
+
   return (
     <div className="p-6 bg-gray-100 dark:bg-dark-bg min-h-[calc(100vh-5vh)] ">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        <Suspense fallback={<CardSkeleton />}>
-          <DashboardSummary type="policies" />
+        {cards.map((card) => (
+          <Suspense key={card.id} fallback={<CardSkeleton />}>
+            <DashboardSummary  count={card.count} title={card.title} />
+          </Suspense>
+        ))}
+        {/* <Suspense fallback={<CardSkeleton />}>
+          <DashboardSummary type={galleries} />
         </Suspense>
         <Suspense fallback={<CardSkeleton />}>
-          <DashboardSummary type="galleries" />
+          <DashboardSummary type={news} />
         </Suspense>
         <Suspense fallback={<CardSkeleton />}>
-          <DashboardSummary type="news" />
-        </Suspense>
-        <Suspense fallback={<CardSkeleton />}>
-          <DashboardSummary type="activities" />
-        </Suspense>
+          <DashboardSummary type={activities}  />
+        </Suspense> */}
       </div>
     </div>
     )
